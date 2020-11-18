46b514c7d36a1383ca29382e69cc7a0dfd4d8433
haerong22 <haerong22@naver.com>
2020-11-19 03:44:38 +0900

    - user api 생성

---
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -0,0 +1,74 @@
+package com.example.study.service;
+
+import com.example.study.ifc.CrudInterface;
+import com.example.study.model.entity.User;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    private final UserRepository userRepository;
+
+    // request data -> user 생성 -> 생성된 데이터 : UserApiResponse return
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // request data
+        UserApiRequest userApiRequest = request.getData();
+        // user 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        // UserApiResponse return
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        // user -> userApiResponse
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // Header + data return
+        return Header.OK(userApiResponse);
+    }
+}
